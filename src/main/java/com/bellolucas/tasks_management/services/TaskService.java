cf1efb7ebd60dfa41e83ea1fdbbb1ab32366f8d4
belloLucas <lucasbellodev@gmail.com>
2025-03-30 09:34:30 -0300

    refactor: improving validation on update method

--- src/main/java/com/bellolucas/tasks_management/services/TaskService.java
+++ src/main/java/com/bellolucas/tasks_management/services/TaskService.java
@@ -1,14 +1,13 @@
 package com.bellolucas.tasks_management.services;
 
 import com.bellolucas.tasks_management.dto.task.CreateTaskDTO;
+import com.bellolucas.tasks_management.dto.task.UpdateTaskDTO;
 import com.bellolucas.tasks_management.entities.task.Task;
 import com.bellolucas.tasks_management.entities.user.User;
 import com.bellolucas.tasks_management.repositories.TaskRepository;
 import com.bellolucas.tasks_management.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
@@ -20,18 +19,56 @@ public class TaskService {
     @Autowired
     private UserRepository userRepository;
 
-    public Task createTask(@RequestBody CreateTaskDTO data) {
+    public Task createTask(CreateTaskDTO data) {
         User assignee = userRepository.findById(data.assignee()).orElseThrow(() -> new IllegalArgumentException("Usuário não encontrado"));
 
         Task task = new Task(data, assignee);
         return taskRepository.save(task);
     }
 
-    public Task getTaskById(@PathVariable Long id) {
-        return taskRepository.findById(id).orElse(null);
+    public Task updateTask(Long id, UpdateTaskDTO data) {
+        Task task = taskRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Tarefa não encontrada"));
+
+        if (data.title() != null) {
+            task.setTitle(data.title());
+        }
+
+        task.setDescription(data.description());
+
+        if (data.priority() != null) {
+            task.setPriority(data.priority());
+        }
+
+        if (data.assignee() != null) {
+            User assignee = userRepository.findById(data.assignee())
+                    .orElseThrow(() -> new IllegalArgumentException("Usuário não encontrado"));
+            task.setAssignee(assignee);
+        }
+
+        if (data.deadline() != null) {
+            task.setDeadline(data.deadline());
+        }
+
+        if (data.completed() != null) {
+            task.setCompleted(data.completed());
+        }
+
+        return taskRepository.save(task);
+    }
+
+    public Task getTaskById(Long id) {
+        return taskRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Tarefa não encontrada"));
     }
 
     public List<Task> getAllTasks() {
         return taskRepository.findAll();
+    }
+
+    public void deleteTask(Long id) {
+        if (!taskRepository.existsById(id)) {
+            throw new IllegalArgumentException("Tarefa não encontrada");
+        }
+        taskRepository.deleteById(id);
     }
 }
