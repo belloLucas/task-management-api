4f9d8f690cb3568658f2a25363674f7e665e1d8b
belloLucas <lucasbellodev@gmail.com>
2025-03-30 10:54:29 -0300

    feat: adding method to find user by email

--- src/main/java/com/bellolucas/tasks_management/repositories/UserRepository.java
+++ src/main/java/com/bellolucas/tasks_management/repositories/UserRepository.java
@@ -3,5 +3,8 @@ package com.bellolucas.tasks_management.repositories;
 import com.bellolucas.tasks_management.entities.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 }
