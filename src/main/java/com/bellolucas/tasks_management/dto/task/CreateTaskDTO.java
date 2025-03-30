e7cfd292f1537084273d04c8247d2260e620c93f
belloLucas <lucasbellodev@gmail.com>
2025-03-30 09:34:50 -0300

    refactor: improving validation on CreateTaskDTO

--- src/main/java/com/bellolucas/tasks_management/dto/task/CreateTaskDTO.java
+++ src/main/java/com/bellolucas/tasks_management/dto/task/CreateTaskDTO.java
@@ -1,26 +1,29 @@
 package com.bellolucas.tasks_management.dto.task;
 
 import com.bellolucas.tasks_management.entities.task.Priority;
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 import java.time.LocalDateTime;
 
 public record CreateTaskDTO(
-        @NotNull
+        @NotBlank(message = "O título é obrigatório")
+        @Size(min = 3, max = 100, message = "O título deve ter entre 3 e 100 caracteres")
         String title,
 
+        @Size(max = 1000, message = "A descrição não pode exceder 1000 caracteres")
         String description,
 
-        @NotNull
+        @NotNull(message = "A prioridade é obrigatória")
         Priority priority,
 
-        @NotNull
+        @NotNull(message = "O responsável é obrigatório")
         Long assignee,
 
-        @NotNull
-        Boolean completed,
-
-        @NotNull
+        @NotNull(message = "A data limite é obrigatória")
+        @FutureOrPresent(message = "A data limite deve ser no presente ou futuro")
         LocalDateTime deadline
 ) {
 }
